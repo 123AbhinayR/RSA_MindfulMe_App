2a2b96ef005bb1961ee15decb5860b65f16cccf2
Nikhil Jayanty <nikhil.jayanty@gmail.com>
2024-03-17 12:56:01 -0400

    ChatBot Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
