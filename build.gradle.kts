da14fba09bd3cae75f794c9032998f4708a2b59b
123AbhinayR <150302252+123AbhinayR@users.noreply.github.com>
2024-03-16 20:02:29 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,8 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+    }
+}
